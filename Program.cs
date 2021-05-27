e50276c4fdbe0002cd3bae8c8f9b57e797759b39
Alistair Evans <alistair.evans@enclave.io>
2021-05-27 12:53:30 +0100

    Add basic program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BundlingIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
